8c3accceab627dd9bc65ca15cc90d6affb664055
Chard33 <jchar33@hotmail.com>
2023-05-18 23:37:04 -0600

    Ajustes

--- ejercicio6/ejercicio6.py
+++ ejercicio6/ejercicio6.py
@@ -1,15 +1,18 @@
 class Vehículo:
 
-    Color = "azul"
-    Ruedas = 4
-    Puertas = 2
+    def __init__(self, Color, Ruedas, Puertas):
+        self.Color = Color
+        self.Ruedas = Ruedas
+        self.Puertas = Puertas
 
 class Coche(Vehículo):
 
-    Velocidad = 140
-    Cilindrada = 4
+    def __init__(self, Color, Ruedas, Puertas, Velocidad, Cilindrada):
+        super().__init__(Color, Ruedas, Puertas)
+        self.Velocidad = Velocidad
+        self.Cilindrada = Cilindrada
 
-miCoche = Coche()
+miCoche = Coche("azul", 4, 4, 150, 1500)
 
 print(f'Mi coche es de color {miCoche.Color}')
 print(f'Mi coche tiene {miCoche.Ruedas} llantas')
