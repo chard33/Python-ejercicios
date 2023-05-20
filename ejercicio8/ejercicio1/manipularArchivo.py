82a6bbf54d6a996eb2a407bca487c286be04e1e4
Chard33 <jchar33@hotmail.com>
2023-05-19 23:25:12 -0600

    Ejercicio 8 python 1

---
+++ ejercicio8/ejercicio1/manipularArchivo.py
@@ -0,0 +1,20 @@
+def main():
+
+    texto = [f'Hola, {input("Ingresa tu nombre: ")}!']
+    archivo = escritura("archivo.txt", texto)
+
+
+def escritura(fichero, texto):
+    
+    archivo = open(fichero, "w")
+
+    for linea in texto:
+        archivo.write(f'{linea}\n')
+
+    archivo.close()
+
+    print("Archivo creado")
+
+if __name__ == "__main__":
+    main()
+
